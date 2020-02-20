e2a554affdec2c1c885235577bcc23c017ba68e8
Syed zaidi <jzaidi@live.com>
2020-02-20 09:06:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+
+ def prime?(int)
+    if int <= 1 
+      return false 
+     else
+      (2..int-1).collect do |number|
+      if int % number == 0
+        return false
+        end
+      end
+    end 
+    true 
+  end
+  
